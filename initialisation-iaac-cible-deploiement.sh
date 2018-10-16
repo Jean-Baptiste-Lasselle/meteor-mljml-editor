4bae48da0dc2c50d5e707b83b70550f9ef752292
Jean-Baptiste-Lasselle <35227860+Jean-Baptiste-Lasselle@users.noreply.github.com>
2018-10-16 16:29:23 +0200

    Create initialisation-iaac-cible-deploiement.sh

---
+++ initialisation-iaac-cible-deploiement.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# - TODO
+
+echo " --------------------------------------------- "
+echo " Ok initialisation IAAC Marguerite Meteor IDE "
+echo " --------------------------------------------- "
