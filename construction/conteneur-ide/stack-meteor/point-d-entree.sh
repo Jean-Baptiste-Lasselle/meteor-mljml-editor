61a110f6d2c5d62a4c147eb15392e392d4d0562b
Jean-Baptiste-Lasselle <35227860+Jean-Baptiste-Lasselle@users.noreply.github.com>
2018-10-17 01:01:33 +0200

    Update point-d-entree.sh

--- construction/conteneur-ide/stack-meteor/point-d-entree.sh
+++ construction/conteneur-ide/stack-meteor/point-d-entree.sh
@@ -2,4 +2,17 @@
 
 export NODE_OPTIONS=$MARGUERITE_NODE_OPTIONS
 export PATH=$PATH:$HOME/.meteor
+
+
+echo " -------------------------------------------------------------- "
+echo " VERIFICATIONS POINT D'ENTREE : "
+echo " -------------------------------------------------------------- "
+echo "  PATH=$PATH  "
+echo "  NODE_OPTIONS=$NODE_OPTIONS  "
+echo "  NOM_PROJET_MARGUERITE_METEOR=$NOM_PROJET_MARGUERITE_METEOR  "
+echo "  Environnement METEOR : "
+env |grep meteor
+echo " -------------------------------------------------------------- "
+
+cd $NOM_PROJET_MARGUERITE_METEOR
 meteor run --port $MARGUERITE_METEOR_PORT
